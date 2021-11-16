8e3fbfb7ee1510e47658c93a6b6f42b4d57c7f59
Jeroen Hof <32430809+Metal-Eagle@users.noreply.github.com>
2021-11-16 07:18:18 +0100

    update

--- src/service/dataBaseConnection.js
+++ src/service/dataBaseConnection.js
@@ -1,25 +1,31 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const userSettings = {
-    userName: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DB,
-    databaseHost: process.env.DATABASE_HOST
-}
+  userName: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  databaseHost: process.env.DATABASE_HOST,
+};
 
 let databaseSettings = {
-    host: userSettings.databaseHost,
-    dialect: 'postgres',
-    logging: false //Remove if logging is usefull
-}
+  host: userSettings.databaseHost,
+  dialect: "postgres",
+  logging: false, //Remove if logging is usefull
+};
 
-if (process.env.NODE_ENV === "production") databaseSettings.logging = false
+if (process.env.NODE_ENV === "production") databaseSettings.logging = false;
 
-module.exports = sequelize = new Sequelize(userSettings.database, userSettings.userName, userSettings.password, databaseSettings)
+module.exports = sequelize = new Sequelize(
+  userSettings.database,
+  userSettings.userName,
+  userSettings.password,
+  databaseSettings
+);
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
