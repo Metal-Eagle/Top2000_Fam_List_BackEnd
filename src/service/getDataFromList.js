7550b01a996fba02a234cfb9f82ee3e59d40dfc7
Jeroen Hof <32430809+Metal-Eagle@users.noreply.github.com>
2021-11-28 09:47:19 +0100

    update to new url

--- src/service/getDataFromList.js
+++ src/service/getDataFromList.js
@@ -1,23 +1,21 @@
-const axios = require('axios')
+const axios = require("axios");
 
 module.exports = getDataFormVoteList = async (url, year) => {
-    if (url.length !== 40) {
-        let urlObject = new URL(url);
-        let urlId = urlObject.pathname.split('/').pop();
-        url = urlId
-    }
+  if (url.length !== 40) {
+    let urlObject = new URL(url);
+    let urlId = urlObject.pathname.split("/").pop();
+    url = urlId;
+  }
 
-    if (url !== null) {
-        if (year === 2019) yearAddon = ''
-        else yearAddon = `-${year}`
-        if (url.length === 40) {
-            let getUrl = `https://stem-backend.npo.nl/api/form/top2000${yearAddon}/${url}`
-            const {
-                data
-            } = await axios.get(getUrl)
+  if (url !== null) {
+    if (year === 2019) yearAddon = "";
+    else yearAddon = `-${year}`;
+    if (url.length === 40) {
+      let getUrl = `https://stem.nporadio2.nl/top2000-2021/share/${url}`;
+      const { data } = await axios.get(getUrl);
 
-            data.year = year
-            return data
-        }
+      data.year = year;
+      return data;
     }
-}
+  }
+};
