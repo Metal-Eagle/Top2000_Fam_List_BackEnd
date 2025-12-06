04c508644ac077f22fc800257c4483b6b577ddf5
Jeroen Hof <32430809+Metal-Eagle@users.noreply.github.com>
2025-12-06 07:20:22 +0100

    Refactor isStringAndHashSame function; remove redundant string comparison and streamline bcrypt usage

--- src/service/decryptService.js
+++ src/service/decryptService.js
@@ -2,14 +2,11 @@ const bcrypt = require('bcrypt');
 
 module.exports = isStringAndHashSame = (string, stringHash) => {
     return new Promise((resolve, reject) => {
-        if (string === stringHash) {
-            resolve(true)
-        }
-        // bcrypt.compare(string, stringHash, (err, result) => {
-        //     if (err) reject(err)
-        //     if (result) resolve(result)
-        //     else reject(result)
-        // });
+        bcrypt.compare(string, stringHash, (err, result) => {
+            if (err) reject(err)
+            if (result) resolve(result)
+            else reject(result)
+        });
     });
 
 }
